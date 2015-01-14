413a31a9e4f8c690f49bce07ca7786900bb10d2a
Roelof Berg <rberg@berg-solutions.de>
2015-01-14 23:35:19 +0100

    Moved from PS3 to xbox controller mapfile.

--- autostart.sh
+++ autostart.sh
@@ -2,4 +2,4 @@
 wakeonlan <ENTER MAC HERE e.g. 12:34:56:78:90:12>
 sleep <ENTER BOOT TIME IN SECONDS HERE>
 cd /home/pi/limelight
-java -jar limelight.jar -mapping PS3.map stream <ENTER IP OR HOSTNAME OF PC HERE>
+java -jar limelight.jar -mapping xbox.map stream <ENTER IP OR HOSTNAME OF PC HERE>
