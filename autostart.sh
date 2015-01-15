8051a1a51157cdb27cc4012013bcc3135aa5f6a1
Roelof Berg <rberg@berg-solutions.de>
2015-01-15 23:57:07 +0100

    Replaced sleep by steam-running-detection.

--- autostart.sh
+++ autostart.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+
+#Start PC
 wakeonlan <ENTER MAC HERE e.g. 12:34:56:78:90:12>
-sleep <ENTER BOOT TIME IN SECONDS HERE>
+
+#Wait until steam is reachable
+echo Connecting to the PC ...
+while ! nc -z -w5 <ENTER IP OR HOSTNAME OF PC HERE> 27036 &>/dev/null; do :; done
+
+#Start limelight
+echo PC available, starting limelight
 cd /home/pi/limelight
 java -jar limelight.jar -mapping xbox.map stream <ENTER IP OR HOSTNAME OF PC HERE>
