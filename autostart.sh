731035c7f6f08e59150a65cfdb5b739a9692981f
Roelof Berg <rberg@berg-solutions.de>
2015-01-16 00:24:47 +0100

    Fixed shield ready detection port no

--- autostart.sh
+++ autostart.sh
@@ -5,7 +5,7 @@ wakeonlan <ENTER MAC HERE e.g. 12:34:56:78:90:12>
 
 #Wait until steam is reachable
 echo Connecting to the PC ...
-while ! nc -z -w5 <ENTER IP OR HOSTNAME OF PC HERE> 27036 &>/dev/null; do :; done
+while ! nc -z -w5 <ENTER IP OR HOSTNAME OF PC HERE> 47989 &>/dev/null; do :; done
 
 #Start limelight
 echo PC available, starting limelight
